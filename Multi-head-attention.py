af3c872046582858cf7758aa369a58e4db740f45
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-08-22 23:53:23 +0530

    done

--- Multi-head-attention.py
+++ Multi-head-attention.py
@@ -21,11 +21,21 @@ class PrePareForMultiHeadAttention(nn.Module):
 
         return x
 class MultiHeadAttention(nn.Module):
-    def __init__(self,d_model:int,heads:int,d_k:int,bias:bool):
+    def __init__(self,heads:int,d_model:int,dropout_prob:float=0.1,bias:bool=True):
+
         super().__init__()
 
-        self.linear=nn.Linear(d_model,heads*d_k,bias=bias)
+        self.d_k=d_model//heads
         self.heads=heads
-        self.d_k=d_k
-    def forward(self,x:torch.Tensor):
-        head_shape=x.shape[:-1]
+
+        self.query=PrePareForMultiHeadAttention(d_model,heads,self.d_k,bias=bias)
+        self.key=PrePareForMultiHeadAttention(d_model,heads,self.d_k,bias=bias)
+        self.value=PrePareForMultiHeadAttention(d_model,heads,self.d_k,bias=True)
+
+
+
+        self.softmax=nn.Softmax(dim=1)
+
+        self.output=nn.Linear(d_model,d_model)
+
+        self.dropout=nn.Dropout(dropout_prob)
