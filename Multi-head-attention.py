0ad5207357b66ae1d3d50772ff053b8f1c28412f
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-08-22 19:48:42 +0530

    done

--- Multi-head-attention.py
+++ Multi-head-attention.py
@@ -1,1 +1,12 @@
 
+import math
+from typing import Optional,List
+
+import torch
+from torch import nn
+
+from labml import tracker
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self,d_model:int,heads:int,d_k:int,bias:bool):
+        super().__init__()
