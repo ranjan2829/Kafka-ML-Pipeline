4091b32d4e04a67afe47d794c69131f5f7a6d69d
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-08-22 23:19:17 +0530

    done

--- Multi-head-attention.py
+++ Multi-head-attention.py
@@ -7,6 +7,25 @@ from torch import nn
 
 from labml import tracker
 
+class PrePareForMultiHeadAttention(nn.Module):
+    def __init__(self,d_model:int,heads:int,d_k:int,bias:bool):
+        super().__init__()
+        self.linear =nn.Linear(d_model,heads*d_k,bias=bias)
+        self.heads=heads
+        self.d_k=d_k
+    def forward(self,x:torch.Tensor):
+        head_shape=x.shape[:-1]
+
+        x=self.linear(x)
+        x=x.view(*head_shape,self.heads,self.d_k)
+
+        return x
 class MultiHeadAttention(nn.Module):
     def __init__(self,d_model:int,heads:int,d_k:int,bias:bool):
         super().__init__()
+
+        self.linear=nn.Linear(d_model,heads*d_k,bias=bias)
+        self.heads=heads
+        self.d_k=d_k
+    def forward(self,x:torch.Tensor):
+        head_shape=x.shape[:-1]
