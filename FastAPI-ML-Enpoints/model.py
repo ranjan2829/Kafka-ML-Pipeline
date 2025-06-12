4668515778480197ccdaefa074c34d3291d205c6
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-06-12 09:03:01 +0530

    done

--- FastAPI-ML-Enpoints/model.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-import joblib
-iris=load_iris()
-x,y=    iris.data,iris.target
-model=RandomForestClassifier()
-model.fit(x,y)
-joblib.dump(model,'model.pkl')
